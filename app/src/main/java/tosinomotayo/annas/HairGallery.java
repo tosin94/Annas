e5dc37a622505028fc63650181bd64b442066efb
tosin94 <tosinomotayo2000@gmail.com>
2017-08-06 01:45:54 +0100

    added new activity

--- app/src/main/java/tosinomotayo/annas/HairGallery.java
+++ app/src/main/java/tosinomotayo/annas/HairGallery.java
@@ -1,29 +1,18 @@
 package tosinomotayo.annas;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+/**
+ * Created by tosinomotayo on 06/08/2017.
+ */
 
 public class HairGallery extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gallery_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
