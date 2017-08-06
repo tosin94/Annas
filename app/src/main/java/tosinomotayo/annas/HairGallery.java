fa2d3c41e0ae43c619c22f4e946024c079cadd00
tosin94 <tosinomotayo2000@gmail.com>
2017-08-06 02:28:12 +0100

    added new activity

--- app/src/main/java/tosinomotayo/annas/HairGallery.java
+++ app/src/main/java/tosinomotayo/annas/HairGallery.java
@@ -1,18 +1,97 @@
 package tosinomotayo.annas;
 
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by tosinomotayo on 06/08/2017.
  */
 
 public class HairGallery extends AppCompatActivity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_layout);
+
+        //set GridView Parameters
+        GridView gallery = (GridView) findViewById(R.id.grid_gallery);
+        gallery.setAdapter(new ImageAdapter(this));//sets the data behind the gridView
+
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {//defining the inner class i will be using
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //a Toast is a view containing a quick little message for the user
+                //TODO add a fragment that will show the image in larger proportions
+                Toast.makeText(HairGallery.this, "" + position, Toast.LENGTH_SHORT).show(); //here no message is being specified
+            }
+        });
     }
+
+    class ImageAdapter extends BaseAdapter{//source of items to be displayed on the grid
+
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+
+
+        @Override
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if(convertView == null){
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+            }
+
+        private Integer[] mThumbIds = {
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7,
+                R.drawable.sample_0, R.drawable.sample_1,
+                R.drawable.sample_2, R.drawable.sample_3,
+                R.drawable.sample_4, R.drawable.sample_5,
+                R.drawable.sample_6, R.drawable.sample_7
+        };
+    }
+
+
 }
+
