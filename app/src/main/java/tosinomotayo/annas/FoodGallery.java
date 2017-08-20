398fa0bedb224a39f3a504546a930e326793f99a
tosin94 <tosinomotayo2000@gmail.com>
2017-08-21 00:13:57 +0100

    starting Food Gallery

--- app/src/main/java/tosinomotayo/annas/FoodGallery.java
+++ app/src/main/java/tosinomotayo/annas/FoodGallery.java
@@ -1,10 +1,140 @@
 package tosinomotayo.annas;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * Created by tosinomotayo on 16/08/2017.
  */
 
 public class FoodGallery extends AppCompatActivity {
+
+    private ActionMode actionMode;
+
+    private static int[] images = {R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7};
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_layout);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.food_toolbar);
+        setSupportActionBar(toolbar);
+
+        final GridView gallery = (GridView) findViewById(R.id.food_grid);
+        gallery.setAdapter(new F_adapter(this));//making the contecxt the foodGallery activity
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);//to allow up navigation
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        } //when nav button is clicked, a call to onOptionsItemSelected() is made
+
+        gallery.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if(modeCallback != null) {
+                    return false;
+                }
+
+                gallery.setSelected(true);
+                return true;
+            }
+
+        });
+
+    }
+
+
+
+    //implementing handler for contextual toolbar
+
+    private ActionMode.Callback modeCallback = new ActionMode.Callback(){
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.context_menu_gallery,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            modeCallback = null;
+
+        }
+    };
+
+    class F_adapter extends BaseAdapter{
+        private Context context;
+
+
+        public F_adapter(Context c){
+
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ImageView imageView;
+
+            if(convertView == null) {
+                imageView = new ImageView(context);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            }
+            else{
+                imageView = (ImageView)convertView;
+            }
+            imageView.setImageResource(images[position]);
+            return imageView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gallery_menu, menu);
+        return true;
+    }
+
 }
