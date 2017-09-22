bc5336c364da6bc7e4950a61dce9da1128ed0df1
tosin94 <tosinomotayo2000@gmail.com>
2017-09-22 17:10:09 +0100

    update to Database

--- app/src/main/java/tosinomotayo/annas/Model.java
+++ app/src/main/java/tosinomotayo/annas/Model.java
@@ -34,24 +34,4 @@ public class Model
         this.data = data;
     }
 
-    class CustomView extends RecyclerView.Adapter{
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-        {
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
-        {
-
-        }
-
-        @Override
-        public int getItemCount()
-        {
-            return 0;
-        }
-    }
 }
